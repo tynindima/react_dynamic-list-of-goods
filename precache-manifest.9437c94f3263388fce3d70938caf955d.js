013a9b3990cbbbd01c04aafdbbe28a31cc5d8282
dima <tynindima@gmail.com>
2020-03-03 09:08:14 +0200

    Updates

---
+++ precache-manifest.9437c94f3263388fce3d70938caf955d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328714ba03bdbf4e720cd5b4970a3749",
+    "url": "./index.html"
+  },
+  {
+    "revision": "181d415d00cd311796dd",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "51c6b2aaca622ca8ff54",
+    "url": "./static/js/2.76693def.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.76693def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181d415d00cd311796dd",
+    "url": "./static/js/main.0bec7277.chunk.js"
+  },
+  {
+    "revision": "2e3a6f278567b71a3edc",
+    "url": "./static/js/runtime-main.5e540f5e.js"
+  }
+]);
